05725fd9eb52798e6a64fb2f41c2c411f09fb332
Gregory Knudsen <dev.artist15@gmail.com>
2017-04-14 16:06:05 -0400

    rename class for dropdown menu

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const mobileNav = (function(){
 	let button = document.querySelector(".menu-button");
-	let menu = document.querySelector(".test");
+	let menu = document.querySelector(".dropdown");
 
 	button.addEventListener("click", function(){
 		console.log("click!");
