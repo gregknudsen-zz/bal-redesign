4b18329d6f6eba4dab13ce1ab2b8b2dc29e9f984
Gregory Knudsen <dev.artist15@gmail.com>
2017-05-23 19:43:03 -0400

    basically a node/express sandbox at this point. Connecting static files (poorly) here

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const serveStatic = require('serve-static');
 
 let port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'))
+
 app.get("/", function(req, res){
-  res.send('<html><head></head><body><h1>HELLO</h1></body></html>')
+  res.send('<html><head><link rel="stylesheet type="text/css" href="css/main.css" /></head><body><h1>HELLO</h1></body></html>')
 });
 
 app.get('/api', function(req, res){
